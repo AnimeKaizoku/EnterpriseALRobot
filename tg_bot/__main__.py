1abc0be28a361682528afbb105e2a8f745767aeb
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-05-31 09:16:28 +0530

    Update __main__.py
    Tryna stop ppl dming me

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,7 +22,7 @@ I am an Anime themed group management bot.
 
 You can find the list of available commands with /help.
 
-[Kigyō's Repo](https://github.com/Dank-del/EnterpriseAL) 
+[Kigyō's Repo](https://github.com/Dank-del/EnterpriseALRobot) 
 
 The support group chat is at @YorktownEagleUnion
 """
@@ -34,7 +34,6 @@ I'm a part of Eagle Union.
 Have a look at the following for an idea of some of \
 the things I can help you with.
 
-I'm managed by [dank_as_fuck](https://t.me/dank_as_fuck)
 AI and blacklist module from [TheRealPhoenixBot](https://github.com/rsktg/TheRealPhoenixBot.git)
 Source code from [SaitamaRobot](https://github.com/AnimeKaizoku/SaitamaRobot)
 
