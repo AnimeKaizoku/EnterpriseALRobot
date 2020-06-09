0a7910688fb6ed6afd5c0e78538b885603e05073
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-06-09 20:58:07 +0530

    Update global_bans.py
    Change pic

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -422,7 +422,7 @@ Note: You can appeal gbans or ask gbans at @YorktownEagleUnion
 
 Kigyo also integrates @Spamwatch API into gbans to remove Spammers as much as possible from your chatroom!
 *What is SpamWatch?*
-SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
+SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/ac12a2c6b831dd005015b.jpg)
 Kigyo will constantly help banning spammers off from your group automatically So, you don't have to worry about spammers storming your group.
 """
 
