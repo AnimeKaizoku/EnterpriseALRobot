15200e14d4f401fcae2d1dfa9d35170aba15a2c0
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-07-01 19:27:03 +0530

    Update anime.py
    im retarded

--- tg_bot/modules/anime.py
+++ tg_bot/modules/anime.py
@@ -8,8 +8,8 @@ import requests
 from telegram import Bot, Update, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
 from telegram.ext import CallbackQueryHandler, run_async
 
-from SaitamaRobot import dispatcher, OWNER_ID, SUDO_USERS, DEV_USERS
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, DEV_USERS
+from tg_bot.modules.disable import DisableAbleCommandHandler
 
 info_btn = "More Information"
 kaizoku_btn = "Kaizoku ☠️"
