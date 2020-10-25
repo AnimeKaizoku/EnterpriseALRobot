524987a381ac84065b45a8053b249f5e20893f8f
Siakmal <69142324+Siakmal@users.noreply.github.com>
2020-10-25 15:17:09 +0800

    Update quote.py

--- tg_bot/modules/quote.py
+++ tg_bot/modules/quote.py
@@ -277,7 +277,7 @@ async def drawer(width, height):
 
 async def fontTest(letter):
         test = ("resources/Roboto-Medium.ttf")
-        for table in tables:
+        
             if ord(letter) in table.cmap.keys():
                 return True
 
