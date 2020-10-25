92794837dd70e7114140d3d35b26887640b3e84b
Siakmal <69142324+Siakmal@users.noreply.github.com>
2020-10-26 06:44:00 +0800

    Update tts.py

--- tg_bot/modules/tts.py
+++ tg_bot/modules/tts.py
@@ -24,7 +24,7 @@ def tts(bot: Bot, update: Update, args):
     update.message.chat.send_action(ChatAction.RECORD_AUDIO)
     lang="ml"
     
-    tts.save("k.mp3")
+    
     with open("k.mp3", "rb") as f:
         linelist = list(f)
         linecount = len(linelist)
