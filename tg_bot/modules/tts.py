389a1c0e8217985edb15fbedb247b8d122d44cd4
Siakmal <69142324+Siakmal@users.noreply.github.com>
2020-10-26 06:28:59 +0800

    Add files via upload

---
+++ tg_bot/modules/tts.py
@@ -0,0 +1,45 @@
+from telegram import ChatAction
+from gtts import gTTS
+import html
+import urllib.request
+import re
+import json
+from datetime import datetime
+from typing import Optional, List
+import time
+import requests
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram import ParseMode
+from telegram.ext import CommandHandler, run_async, Filters
+from telegram.utils.helpers import escape_markdown, mention_html
+from chizuru import dispatcher
+from chizuru.__main__ import STATS
+from chizuru.modules.disable import DisableAbleCommandHandler
+from chizuru.modules.helper_funcs.extraction import extract_user
+
+def tts(bot: Bot, update: Update, args):
+    current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
+    filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
+    reply = " ".join(args)
+    update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+    lang="ml"
+    tts = gTTS(reply, lang)
+    tts.save("k.mp3")
+    with open("k.mp3", "rb") as f:
+        linelist = list(f)
+        linecount = len(linelist)
+    if linecount == 1:
+        update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+        lang = "en"
+        tts = gTTS(reply, lang)
+        tts.save("k.mp3")
+    with open("k.mp3", "rb") as speech:
+        update.message.reply_voice(speech, quote=False)
+
+
+    __help__ = """ Text to speech
+    - /tts <your text>
+    """
+    __mod_name__ = "tts"
+
+dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
