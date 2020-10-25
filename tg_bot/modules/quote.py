6e0423911c3a88dbf48c3421fbd46d5851b522f1
Siakmal <69142324+Siakmal@users.noreply.github.com>
2020-10-25 14:51:55 +0800

    Update quote.py

--- tg_bot/modules/quote.py
+++ tg_bot/modules/quote.py
@@ -12,7 +12,7 @@ import random
 import json
 import os
 import re
-from chizuru.lyn import chizurubot
+from tg_bot.lyn import tg_botbot
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
@@ -374,7 +374,7 @@ async def replied_user(draw, tot, text, maxlength, title):
                 draw.text((180 + space, 132), letter, font=textfont, fill="white")
                 space += textfont.getsize(letter)[0]
                 
-@chizurubot(pattern="^/q")
+@tg_botbot(pattern="^/q")
 async def _(event):
     if event.fwd_from:
         return
