0267b339d3465b2ca390c0f280ea6496b5b44179
Chizuru <chizuru@kanojo.tk>
2020-10-30 18:54:14 +0530

    Update filters.py
    Co-Authored-By: Stɑrry Shivɑm <58552395+starry69@users.noreply.github.com>

--- tg_bot/modules/helper_funcs/filters.py
+++ tg_bot/modules/helper_funcs/filters.py
@@ -1,40 +1,55 @@
 from telegram import Message
-from telegram.ext import BaseFilter
+from telegram.ext import MessageFilter
 
 from tg_bot import SUPPORT_USERS, SUDO_USERS, DEV_USERS
 
 
 class CustomFilters(object):
-    class _Supporters(BaseFilter):
+    class _Supporters(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
+            return bool(
+                message.from_user and message.from_user.id in SUPPORT_USERS
+            )
 
     support_filter = _Supporters()
 
-    class _Sudoers(BaseFilter):
+    class _Sudoers(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUDO_USERS)
+            return bool(
+                message.from_user and message.from_user.id in SUDO_USERS
+            )
 
     sudo_filter = _Sudoers()
-
-    class _Developers(BaseFilter):
+    
+    class _Developers(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in DEV_USERS)
+            return bool(
+                message.from_user and message.from_user.id in DEV_USERS
+            )
 
     dev_filter = _Developers()
 
-    class _MimeType(BaseFilter):
+    class _MimeType(MessageFilter):
         def __init__(self, mimetype):
             self.mime_type = mimetype
             self.name = "CustomFilters.mime_type({})".format(self.mime_type)
 
         def filter(self, message: Message):
-            return bool(message.document and message.document.mime_type == self.mime_type)
+            return bool(
+                message.document
+                and message.document.mime_type == self.mime_type
+            )
 
     mime_type = _MimeType
 
-    class _HasText(BaseFilter):
+    class _HasText(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.text or message.sticker or message.photo or message.document or message.video)
+            return bool(
+                message.text
+                or message.sticker
+                or message.photo
+                or message.document
+                or message.video
+            )
 
     has_text = _HasText()
