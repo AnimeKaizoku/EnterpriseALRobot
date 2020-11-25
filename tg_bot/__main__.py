ca9780109683a69c2d95df03a442ac9376ff4b2e
ダンクデル // 千鶴 <chizuru@kanojo.tk>
2020-11-26 01:33:40 +0530

    comment out useless print.

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -223,7 +223,7 @@ def help_button(update, context):
     next_match = re.match(r"help_next\((.+?)\)", query.data)
     back_match = re.match(r"help_back", query.data)
 
-    print(query.message.chat.id)
+    # print(query.message.chat.id)
 
     try:
         if mod_match:
