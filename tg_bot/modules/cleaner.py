9e4e796eebed06e980562288ee466333dd9c671c
Chizuru <chizuru@kanojo.tk>
2021-01-14 22:29:12 +0530

    [modnames] shortened a few to align the buttons.

--- tg_bot/modules/cleaner.py
+++ tg_bot/modules/cleaner.py
@@ -263,7 +263,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "Bluetext Cleaning"
+__mod_name__ = "Cleaner"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
