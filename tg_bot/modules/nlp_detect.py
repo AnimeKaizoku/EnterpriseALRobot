3755b11b5e8674bfcaa4c7352a78999408b089af
Chizuru <chizuru@kanojo.tk>
2021-02-08 01:45:59 +0530

    nlp : check admin on /nlpstat
    Co-Authored-By: ポキ <poki@pokurt.me>

--- tg_bot/modules/nlp_detect.py
+++ tg_bot/modules/nlp_detect.py
@@ -1,51 +1,75 @@
 from pyrogram import filters
 from tg_bot import kp, CF_API_KEY, log
 from pyrogram.handlers import MessageHandler
-from pyrogram.types import ChatPermissions
+from pyrogram.types import ChatPermissions, Message
 from pyrogram.errors import BadRequest
 import requests
 from tg_bot.modules.global_bans import SPB_MODE
 import tg_bot.modules.sql.nlp_detect_sql as sql
 from tg_bot.modules.language import gs
 
+from pyrogram.types import Message
+
+
+async def admin_check(message: Message) -> bool:
+    client = message._client
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    check_status = await client.get_chat_member(
+        chat_id=chat_id,
+        user_id=user_id
+    )
+    admin_strings = [
+        "creator",
+        "administrator"
+    ]
+    return check_status.status in admin_strings
+
 __mod_name__ = "NLP"
 
 def get_help(chat):
     return gs(chat, "nlp_help")
 
+
 @kp.on_message(filters.command("nlpstat"), group=8)
 async def nlp_mode(client, message):
-    print("ok")
+    is_admin = await admin_check(message)
     args = message.text.split(None, 1)
-    if len(args) > 1:
-        if args[1].lower() in ["on", "yes"]:
-            sql.enable_nlp(message.chat.id)
+
+    if is_admin == True:
+        if len(args) > 1:
+            if args[1].lower() in ["on", "yes"]:
+                sql.enable_nlp(message.chat.id)
+                await message.reply_text(
+                    "I've enabled NLP moderation in this group. This will help protect you "
+                    "from spammers, unsavoury characters, and the biggest trolls."
+                )
+            elif args[1].lower() in ["off", "no"]:
+                sql.disable_nlp(message.chat.id)
+                await message.reply_text(
+                    "I've disabled NLP moderation in this group. NLP wont affect your users "
+                    "anymore. You'll be less protected from any trolls and spammers "
+                    "though!"
+                )
+        else:
             await message.reply_text(
-                "I've enabled NLP moderation in this group. This will help protect you "
-                "from spammers, unsavoury characters, and the biggest trolls."
-            )
-        elif args[1].lower() in ["off", "no"]:
-            sql.disable_nlp(message.chat.id)
-            await message.reply_text(
-                "I've disabled NLP moderation in this group. NLP wont affect your users "
-                "anymore. You'll be less protected from any trolls and spammers "
-                "though!"
+                "Give me some arguments to choose a setting! on/off, yes/no!\n\n"
+                "Your current setting is: {}\n"
+                "When True, any messsages will go through NLP and spammers will be banned.\n"
+                "When False, they won't, leaving you at the possible mercy of spammers\n"
+                "NLP powered by @Intellivoid.".format(sql.does_chat_nlp(message.chat.id))
             )
     else:
-        await message.reply_text(
-            "Give me some arguments to choose a setting! on/off, yes/no!\n\n"
-            "Your current setting is: {}\n"
-            "When True, any messsages will go through NLP and spammers will be banned. "
-            "When False, they won't, leaving you at the possible mercy of spammers "
-            "NLP powered by @Intellivoid.".format(sql.does_chat_nlp(message.chat.id))
-        )
+        await message.reply_text("You aren't an admin.")
+
 
 @kp.on_message(filters.text & filters.group, group=3)
 async def detect_spam(client, message):
     user = message.from_user
     chat = message.chat
     chat_state = sql.does_chat_nlp(chat.id)
-    if SPB_MODE and CF_API_KEY and chat_state == True:
+    if SPB_MODE:
         try:
             result = requests.get(f'https://api.intellivoid.net/coffeehouse/v1/nlp/spam_prediction/chatroom?input={message.text}',params={'access_key' : CF_API_KEY})
             res_json = result.json()
