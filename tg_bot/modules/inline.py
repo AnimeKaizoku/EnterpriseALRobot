72f5689312d68ab72f7cafaa0a935164b2d7b892
Sayan Biswas <sayan@pokurt.me>
2021-03-23 16:37:41 +0530

    inline: fix anime query.

--- tg_bot/modules/inline.py
+++ tg_bot/modules/inline.py
@@ -341,7 +341,6 @@ def spb(query: str, update: Update, context: CallbackContext) -> None:
     update.inline_query.answer(results, cache_time=5)
 
 
-# Anilist stuff begins // queries written by github.com/the-blank-x (t.me/TheKneesocks) // parser by github.com/Dank-del (t.me/dank_as_fuck)
 
 MEDIA_QUERY = '''query ($search: String) {
   Page (perPage: 10) {
@@ -357,6 +356,7 @@ MEDIA_QUERY = '''query ($search: String) {
       status
       description
       episodes
+      bannerImage
       duration
       chapters
       volumes
@@ -383,10 +383,9 @@ def media_query(query: str, update: Update, context: CallbackContext) -> None:
     results: List = []
 
     try:
-        search = query.split(" ", 1)[1]
         results: List = []
         r = requests.post('https://graphql.anilist.co',
-                          data=json.dumps({'query': MEDIA_QUERY, 'variables': {'search': search}}),
+                          data=json.dumps({'query': MEDIA_QUERY, 'variables': {'search': query}}),
                           headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
         res = r.json()
         data = res['data']['Page']['media']
@@ -396,6 +395,7 @@ def media_query(query: str, update: Update, context: CallbackContext) -> None:
             title_ja = data["title"].get("romaji") or "N/A"
             format = data.get("format") or "N/A"
             type = data.get("type") or "N/A"
+            bannerimg = data.get("bannerImage") or "https://telegra.ph/file/cc83a0b7102ad1d7b1cb3.jpg"
             try:
                 des = data.get("description").replace("<br>", "").replace("</br>", "")
                 description = des.replace("<i>", "").replace("</i>", "") or "N/A"
@@ -408,13 +408,13 @@ def media_query(query: str, update: Update, context: CallbackContext) -> None:
                 description = description or "N/A"
 
             if len((str(description))) > 700:
-                description = description[0:700] + "....."
+                description = description [0:700] + "....."
 
             avgsc = data.get("averageScore") or "N/A"
             status = data.get("status") or "N/A"
             genres = data.get("genres") or "N/A"
             genres = ", ".join(genres)
-            img = f"https://img.anili.st/media/{data['id']}" or ""
+            img = f"https://img.anili.st/media/{data['id']}" or "https://telegra.ph/file/cc83a0b7102ad1d7b1cb3.jpg"
             aurl = data.get("siteUrl")
             kb = InlineKeyboardMarkup(
                 [
@@ -448,7 +448,7 @@ def media_query(query: str, update: Update, context: CallbackContext) -> None:
                     reply_markup=kb
                 )
             )
-    except (IndexError):
+    except Exception as e:
         kb = InlineKeyboardMarkup(
             [
                 [
@@ -465,7 +465,7 @@ def media_query(query: str, update: Update, context: CallbackContext) -> None:
                 (
                 id=str(uuid4()),
                 title=f"Media {query} not found",
-                input_message_content=InputTextMessageContent(f"Media {query} not found", parse_mode=ParseMode.MARKDOWN,
+                input_message_content=InputTextMessageContent(f"Media {query} not found due to {e}", parse_mode=ParseMode.MARKDOWN,
                                                               disable_web_page_preview=True),
                 reply_markup=kb
             )
