e94a19d64a4e45abc1b0cf2f042f45e314464dd5
Sayan Biswas <sayan@pokurt.me>
2021-05-12 00:43:32 +0530

    stickers: remove sticker search.
    Well i dont like cloudfare hackeroo.

--- tg_bot/modules/stickers.py
+++ tg_bot/modules/stickers.py
@@ -1,22 +1,16 @@
-import os
-import re
 import math
-import requests
 import urllib.request as urllib
-from urllib.error import HTTPError
-from io import BytesIO
-from PIL import Image
 from html import escape
-from bs4 import BeautifulSoup as bs
+from io import BytesIO
+from urllib.error import HTTPError
 
-from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram import TelegramError, Update
+from PIL import Image
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
+                      TelegramError, Update)
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 from tg_bot.modules.helper_funcs.decorators import kigcmd
-from tg_bot import dispatcher
 
-combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
 @kigcmd(command='stickerid')
 def stickerid(update: Update, context: CallbackContext):
@@ -38,25 +32,6 @@ def stickerid(update: Update, context: CallbackContext):
             parse_mode=ParseMode.HTML,
         )
 
-@kigcmd(command='stickers')
-def cb_sticker(update: Update, context: CallbackContext):
-    msg = update.effective_message
-    split = msg.text.split(" ", 1)
-    if len(split) == 1:
-        msg.reply_text("Provide some name to search for pack.")
-        return
-    text = requests.get(combot_stickers_url + split[1]).text
-    soup = bs(text, "lxml")
-    results = soup.find_all("a", {"class": "sticker-pack__btn"})
-    titles = soup.find_all("div", "sticker-pack__title")
-    if not results:
-        msg.reply_text("No results found :(.")
-        return
-    reply = f"Stickers for *{split[1]}*:"
-    for result, title in zip(results, titles):
-        link = result["href"]
-        reply += f"\n• [{title.get_text()}]({link})"
-    msg.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
 
 @kigcmd(command='getsticker')
 def getsticker(update: Update, context: CallbackContext):
