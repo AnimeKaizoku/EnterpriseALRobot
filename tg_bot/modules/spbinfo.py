51af1c5beacc725b9420d9edb88e789013c02c68
Sayan Biswas <sayan@pokurt.me>
2021-07-01 19:18:54 +0530

    spbinfo: rework

--- tg_bot/modules/spbinfo.py
+++ tg_bot/modules/spbinfo.py
@@ -8,43 +8,47 @@ def lookup(update, context):
     message = update.effective_message
     args = context.args
     user_id = extract_user(message, args)
+    if user_id is None:
+        user_id = update.effective_user.id
     url = f"https://api.intellivoid.net/spamprotection/v1/lookup?query={user_id}"
+    #print(url)
     r = requests.get(url)
     a = r.json()
-    response = a["success"]
+    #print(a)
+    response = a.get('success')
     if response is True:
-        date = a["results"]["last_updated"]
+        date = a.get("results").get("last_updated")
         stats = f"**◢ Intellivoid• SpamProtection Info**:\n"
         stats += f' • **Updated on**: `{datetime.datetime.fromtimestamp(date).strftime("%Y-%m-%d %I:%M:%S %p")}`\n'
         stats += (
             f" • **Chat Info**: [Link](t.me/SpamProtectionBot/?start=00_{user_id})\n"
         )
 
-        if a["results"]["attributes"]["is_potential_spammer"] is True:
+        if a.get("results").get("attributes").get("is_potential_spammer") is True:
             stats += f" • *User*: `USERxSPAM`\n"
-        elif a["results"]["attributes"]["is_operator"] is True:
+        elif a.get("results").get("attributes").get("is_operator") is True:
             stats += f" • *User*: `USERxOPERATOR`\n"
-        elif a["results"]["attributes"]["is_agent"] is True:
+        elif a.get("results").get("attributes").get("is_agent") is True:
             stats += f" • *User*: `USERxAGENT`\n"
-        elif a["results"]["attributes"]["is_whitelisted"] is True:
+        elif a.get("results").get("attributes").get("is_whitelisted") is True:
             stats += f" • *User*: `USERxWHITELISTED`\n"
 
-        stats += f' • *Type*: `{a["results"]["entity_type"]}`\n'
+        stats += f' • *Type*: `{a.get("results").get("entity_type")}`\n'
         stats += (
-            f' • *Language*: `{a["results"]["language_prediction"]["language"]}`\n'
+            f' • *Language*: `{a.get("results").get("language_prediction").get("language")}`\n'
         )
-        stats += f' • *Language Probability*: `{a["results"]["language_prediction"]["probability"]}`\n'
+        stats += f' • *Language Probability*: `{a.get("results").get("language_prediction").get("probability")}`\n'
         stats += f"*Spam Prediction*:\n"
-        stats += f' • *Ham Prediction*: `{a["results"]["spam_prediction"]["ham_prediction"]}`\n'
-        stats += f' • *Spam Prediction*: `{a["results"]["spam_prediction"]["spam_prediction"]}`\n'
-        stats += f'*Blacklisted*: `{a["results"]["attributes"]["is_blacklisted"]}`\n'
-        if a["results"]["attributes"]["is_blacklisted"] is True:
+        stats += f' • *Ham Prediction*: `{a.get("results").get("spam_prediction").get("ham_prediction")}`\n'
+        stats += f' • *Spam Prediction*: `{a.get("results").get("spam_prediction").get("spam_prediction")}`\n'
+        stats += f'*Blacklisted*: `{a.get("results").get("attributes").get("is_blacklisted")}`\n'
+        if a.get("results").get("attributes").get("is_blacklisted") is True:
             stats += (
-                f' • *Reason*: `{a["results"]["attributes"]["blacklist_reason"]}`\n'
+                f' • *Reason*: `{a.get("results").get("attributes").get("blacklist_reason")}`\n'
             )
-            stats += f' • *Flag*: `{a["results"]["attributes"]["blacklist_flag"]}`\n'
-        stats += f'*PTID*:\n`{a["results"]["private_telegram_id"]}`\n'
+            stats += f' • *Flag*: `{a.get("results").get("attributes").get("blacklist_flag")}`\n'
+        stats += f'*PTID*:\n`{a.get("results").get("private_telegram_id")}`\n'
         message.reply_text(stats, parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
     else:
-        message.reply_text("`cannot reach SpamProtection API`")
+        message.reply_text("`cannot reach SpamProtection API`", parse_mode=telegram.ParseMode.MARKDOWN)
         time.sleep(3)
