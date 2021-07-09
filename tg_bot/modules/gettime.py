b8e640f8f61a54da1358a73f55bc884f5c841785
Sayan Biswas <sayan@pokurt.me>
2021-07-09 14:31:50 +0530

    gettime: improve condition check

--- tg_bot/modules/gettime.py
+++ tg_bot/modules/gettime.py
@@ -23,11 +23,7 @@ def generate_time(to_find: str, findtype: List[str]) -> str:
                 country_zone = zone["zoneName"]
                 country_code = zone["countryCode"]
 
-                if zone["dst"] == 1:
-                    daylight_saving = "Yes"
-                else:
-                    daylight_saving = "No"
-
+                daylight_saving = "Yes" if zone["dst"] == 1 else "No"
                 date_fmt = r"%d-%m-%Y"
                 time_fmt = r"%H:%M:%S"
                 day_fmt = r"%A"
