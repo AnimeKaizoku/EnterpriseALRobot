12cda872d94c876baee77bf02bd46c7a1495afa9
Sayan Biswas <sayan@pokurt.me>
2021-07-09 14:40:36 +0530

    users: improve condition check

--- tg_bot/modules/users.py
+++ tg_bot/modules/users.py
@@ -38,9 +38,7 @@ def get_user_id(username):
                     return userdat.id
 
             except BadRequest as excp:
-                if excp.message == "Chat not found":
-                    pass
-                else:
+                if excp.message != "Chat not found":
                     log.exception("Error extracting user ID")
 
     return None
@@ -123,7 +121,7 @@ def chats(update: Update, context: CallbackContext):
             chatfile += "{}. {} | {} | {}\n".format(
                 P, chat.chat_name, chat.chat_id, chat_members
             )
-            P = P + 1
+            P += 1
         except:
             pass
 
