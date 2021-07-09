6377ee086eee35e6d20df04f50e3d26639b658fd
Sayan Biswas <sayan@pokurt.me>
2021-07-09 14:32:22 +0530

    language: improve condition check

--- tg_bot/modules/language.py
+++ tg_bot/modules/language.py
@@ -20,7 +20,7 @@ def paginate(
             itertools.islice(i1, page_size, None),
             list(itertools.islice(i2, page_size)),
         )
-        if len(page) == 0:
+        if not page:
             break
         yield page
 
@@ -39,15 +39,10 @@ def set_lang(update: Update, _) -> None:
         get_language(sql.get_chat_lang(chat.id))[:-3]
     )
 
-    keyb = []
-    for code, name in get_languages().items():
-        keyb.append(
-            InlineKeyboardButton(
+    keyb = [InlineKeyboardButton(
                 text=name,
                 callback_data=f"setLang_{code}",
-            )
-        )
-
+            ) for code, name in get_languages().items()]
     keyb = list(paginate(keyb, 2))
     keyb.append(
         [
