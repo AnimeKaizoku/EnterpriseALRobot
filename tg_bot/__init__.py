ade707a99acd4f8727c6f87e04160642ece22f5d
Sayan Biswas <sayan@pokurt.me>
2021-09-19 18:11:47 +0530

    init: use args in postgrespersistence.
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -133,7 +133,7 @@ sw = KInit.init_sw()
 from tg_bot.modules.sql import SESSION
 
 
-updater = tg.Updater(TOKEN, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10}, persistence=PostgresPersistence(SESSION))
+updater = tg.Updater(TOKEN, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10}, persistence=PostgresPersistence(session=SESSION))
 telethn = TelegramClient(MemorySession(), APP_ID, API_HASH)
 dispatcher = updater.dispatcher
 
