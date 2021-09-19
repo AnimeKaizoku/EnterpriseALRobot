00043de7ef3dab99450b3027d09b5d0fd6eea6e9
LightLegendXR <83782017+LightLegendXR@users.noreply.github.com>
2021-09-19 18:31:35 -0400

    Update nations.py

--- tg_bot/modules/nations.py
+++ tg_bot/modules/nations.py
@@ -382,16 +382,16 @@ def removesardegna(update: Update, context: CallbackContext) -> str:
         return ""
 
 # I added extra new lines
-nations = """ Kigyō has bot access levels we call as *"Nation Levels"*
+nations = """ Zhongli has bot access levels we call as *"Nation Levels"*
 \n*Eagle Union* - Devs who can access the bots server and can execute, edit, modify bot code. Can also manage other Nations
 \n*God* - Only one exists, bot owner.
-Owner has complete bot access, including bot adminship in chats Kigyō is at.
-\n*Royals* - Have super user access, can gban, manage Nations lower than them and are admins in Kigyō.
-\n*Sakuras* - Have access go globally ban users across Kigyō.
+Owner has complete bot access, including bot adminship in chats Zhongli is at.
+\n*Royals* - Have super user access, can gban, manage Nations lower than them and are admins in Zhongli.
+\n*Sakuras* - Have access go globally ban users across Zhongli.
 \n*Sardegnas* - Same as Neptunians but can unban themselves if banned.
 \n*Neptunians* - Cannot be banned, muted flood kicked but can be manually banned by admins.
-\n*Disclaimer*: The Nation levels in Kigyō are there for troubleshooting, support, banning potential scammers.
-Report abuse or ask us more on these at [Eagle Union](https://t.me/YorktownEagleUnion).
+\n*Disclaimer*: The Nation levels in Zhongli are there for troubleshooting, support, banning potential scammers.
+Report abuse or ask us more on these at [Support Group](https://t.me/joinchat/EII_nkvf5543MGVk).
 """
 
 
