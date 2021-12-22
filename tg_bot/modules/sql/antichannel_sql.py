7c28e97bc5cdd8ccbfc705ade17e0d354ab976bb
Sayan Biswas <sayan@pokurt.me>
2021-12-22 16:12:14 +0530

    antichannel_sql.py: fix types
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- tg_bot/modules/sql/antichannel_sql.py
+++ tg_bot/modules/sql/antichannel_sql.py
@@ -9,7 +9,7 @@ from tg_bot.modules.sql import BASE, SESSION
 
 class AntiChannelSettings(BASE):
     __tablename__ = "anti_channel_settings"
-    
+
     chat_id = Column(String(14), primary_key=True)
     setting = Column(Boolean, default=False, nullable=False)
 
@@ -24,11 +24,12 @@ class AntiChannelSettings(BASE):
 AntiChannelSettings.__table__.create(checkfirst=True)
 ANTICHANNEL_SETTING_LOCK = threading.RLock()
 
+
 def enable_antichannel(chat_id: int):
     with ANTICHANNEL_SETTING_LOCK:
         chat = SESSION.query(AntiChannelSettings).get(str(chat_id))
         if not chat:
-            chat = AntiChannelSettings(str(chat_id), True)
+            chat = AntiChannelSettings(chat_id, True)
 
         chat.setting = True
         SESSION.add(chat)
@@ -39,7 +40,7 @@ def disable_antichannel(chat_id: int):
     with ANTICHANNEL_SETTING_LOCK:
         chat = SESSION.query(AntiChannelSettings).get(str(chat_id))
         if not chat:
-            chat = AntiChannelSettings(str(chat_id), False)
+            chat = AntiChannelSettings(chat_id, False)
 
         chat.setting = False
         SESSION.add(chat)
@@ -52,8 +53,6 @@ def antichannel_status(chat_id: int) -> bool:
         if not d:
             return False
         return d.setting
-
-
 
 
 def migrate_chat(old_chat_id, new_chat_id):
