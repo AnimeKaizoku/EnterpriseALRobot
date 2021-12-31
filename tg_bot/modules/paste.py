05913b31e2c967e339c7f4ef6a4fd4dffab43a9e
Sayan Biswas <sayan@intellivoid.net>
2022-01-01 01:54:29 +0530

    /paste: get files and upload to privatebin
    Signed-off-by: Sayan Biswas <sayan@intellivoid.net>

--- tg_bot/modules/paste.py
+++ tg_bot/modules/paste.py
@@ -1,7 +1,8 @@
-import requests
-from telegram import ParseMode, Update
+from .helper_funcs.misc import upload_text
+from telegram import Update
 from telegram.ext import CallbackContext
 from tg_bot.modules.helper_funcs.decorators import kigcmd
+from io import BytesIO
 
 @kigcmd(command='paste', pass_args=True)
 def paste(update: Update, context: CallbackContext):
@@ -9,26 +10,25 @@ def paste(update: Update, context: CallbackContext):
     message = update.effective_message
 
     if message.reply_to_message:
-        data = message.reply_to_message.text
+        data = message.reply_to_message.text or message.reply_to_message.caption
+        if message.reply_to_message.document:
+            file_info = context.bot.get_file(message.reply_to_message.document.file_id)
+            with BytesIO() as file:
+                file_info.download(out=file)
+                file.seek(0)
+                data = file.read().decode()
 
     elif len(args) >= 1:
         data = message.text.split(None, 1)[1]
-
     else:
         message.reply_text("What am I supposed to do with this?")
         return
+    
+    txt = ""
+    paste_url = upload_text(data)
+    if not paste_url:
+        txt = "Failed to paste data"
+    else:
+        txt = "Successfully uploaded to Privatebin: {}".format(paste_url)
 
-    key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": data})
-        .json()
-        .get("result")
-        .get("key")
-    )
-
-    url = f"https://nekobin.com/{key}"
-
-    reply_text = f"Nekofied to *Nekobin* : {url}"
-
-    message.reply_text(
-        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-    )
+    message.reply_text(txt, disable_web_page_preview=True)
