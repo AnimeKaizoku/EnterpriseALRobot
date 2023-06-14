860bb34c33c585fd58f4996dca3e0c096cf4e317
CyberLink_IB13 <99059976+IzanagiBakura@users.noreply.github.com>
2023-06-14 12:19:02 -0400

    Create wiki.py

---
+++ tg_bot/modules/wiki.py
@@ -0,0 +1,64 @@
+import wikipedia
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+from wikipedia.exceptions import DisambiguationError, PageError
+
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+
+language = "es"
+wikipedia.set_lang(language)
+
+@run_async
+def wiki(update: Update, context: CallbackContext):
+    msg = (
+        update.effective_message.reply_to_message
+        if update.effective_message.reply_to_message
+        else update.effective_message
+    )
+    res = ""
+    if msg == update.effective_message:
+        search = msg.text.split(" ", maxsplit=1)[1]
+    else:
+        search = msg.text
+    try:
+        res = wikipedia.summary(search)
+    except DisambiguationError as e:
+        update.message.reply_text(
+            "Páginas desambiguadas encontradas! Adapte su consulta en consecuencia.\n<i>{}</i>".format(
+                e
+            ),
+            parse_mode=ParseMode.HTML,
+        )
+    except PageError as e:
+        update.message.reply_text(
+            "<code>{}</code>".format(e), parse_mode=ParseMode.HTML
+        )
+    if res:
+        result = f"<b>{search}</b>\n\n"
+        result += f"<i>{res}</i>\n"
+        result += f"""<a href="https://es.wikipedia.org/wiki/{search.replace(" ", "%20")}">Leer más...</a>"""
+        if len(result) > 4000:
+            with open("result.txt", "w") as f:
+                f.write(f"{result}\n\nUwU OwO OmO UmU")
+            with open("result.txt", "rb") as f:
+                context.bot.send_document(
+                    document=f,
+                    filename=f.name,
+                    reply_to_message_id=update.message.message_id,
+                    chat_id=update.effective_chat.id,
+                    parse_mode=ParseMode.HTML,
+                )
+        else:
+            update.message.reply_text(
+                result, parse_mode=ParseMode.HTML, disable_web_page_preview=True
+            )
+
+
+WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
+dispatcher.add_handler(WIKI_HANDLER)
+
+__help__ = """
+» /wiki (text) *:* Busca sobre el texto dado en wikipedia.
+"""
+__mod_name__ = "Wiki"
